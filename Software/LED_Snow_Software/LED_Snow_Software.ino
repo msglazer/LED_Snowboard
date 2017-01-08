5344c345c3f8fe7e47d8585b7f74106eb9f34aff
msglazer <msg1254@rit.edu>
2017-01-07 19:33:19 -0500

    Continued work on software

--- Software/LED_Snow_Software/LED_Snow_Software.ino
+++ Software/LED_Snow_Software/LED_Snow_Software.ino
@@ -1,33 +1,46 @@
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <TinyGPS++.h>
 #include <MahonyAHRS.h>
 #include <SparkFunLSM9DS1.h>
-#include <Wire.h>
-#include <FastLED.h>
+//#include <FastLED.h>
+#include <Adafruit_NeoPixel.h>
 
 
 #define NUM_LEDS    60
 #define COLOR_ORDER GRB
 #define Brightness  128
-#define LSM9DS1_M   0x58  //change this 
-#define LSM9DS1_AG  0x58  //change this 
-#define DECLINATION 14.45
-#define LED_PIN     16     
+#define LSM9DS1_M   0x1E  
+#define LSM9DS1_AG  0x6B   
+#define DECLINATION -14.45
+#define LED_PIN     16  
+#define OLED_RESET  17  
+#define ROTATION    2
+#define voltagePin  15
 
 
-CRGB leds[NUM_LEDS];
+//CRGB leds[NUM_LEDS];
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 LSM9DS1 imu;
 TinyGPSPlus gps;
 Mahony filter;
-
+Adafruit_SSD1306 display(OLED_RESET);
 
 HardwareSerial Uart = HardwareSerial();
 
 
 int Ipos   = NUM_LEDS / 2;
 int width  = 10;
-float GPSlat, GPSlng, GPSspeed;
+float R1   = 10;    //change these values
+float R2   = 10;    //change these values
+float GPSlat, GPSlng, GPSspeed, maxSpeed, GPSheightF, GPSheightM;
 float roll, pitch, heading;
+float voltage;
+uint32_t LEDcolor;
+const int chipSelect = BUILTIN_SDCARD;
 
 
 void setup() 
@@ -37,14 +50,34 @@ void setup()
   Serial.begin(115200);
   Uart.begin(9600);
 
+  
+  Serial.print("Initializing SD card...");
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+
+
+  display.clearDisplay();         // Clear the display.
+  display.display();              
+  display.setRotation(ROTATION);  // Set rotation.
+  display.setTextSize(2);         // Set text size and color.
+  display.setTextColor(WHITE);
+
   imu.settings.device.commInterface = IMU_MODE_I2C;
   imu.settings.device.mAddress = LSM9DS1_M;
   imu.settings.device.agAddress = LSM9DS1_AG;
   imu.begin();
   filter.begin(100);
-  
-  FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(leds,NUM_LEDS).setCorrection(TypicalSMD5050);
-  FastLED.setBrightness (Brightness);
+
+  strip.begin();
+  strip.show();
+  //FastLED.addLeds<WS2812B, LED_PIN, COLOR_ORDER>(leds,NUM_LEDS).setCorrection(TypicalSMD5050);
+  //FastLED.setBrightness (Brightness);
 }
 
 void loop()
@@ -59,6 +92,11 @@ void loop()
     while(true);
   }
 
+  //need to actually call some functions here
+  speedDistance();
+  sensorFusion();
+  batteryV();
+  snow_show();
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
@@ -78,12 +116,20 @@ void displayInfo()
   }
   else if (gps.altitude.isUpdated())
   {
-    gps.altitude.meters();
-    gps.altitude.feet();
+    GPSheightM = gps.altitude.meters();
+    GPSheightF = gps.altitude.feet();
   }
 }
 
+void speedDistance()
+{
+  if (GPSspeed > maxSpeed)
+  {
+    maxSpeed = GPSspeed;
+  }
 
+  //implement distance stuff here if interested
+}
 
 /////////////////////////////////////////////////////////////////////////////////////
 ////////////                           IMU Functions                     ////////////
@@ -123,12 +169,13 @@ void sensorFusion()
 ////////////                           LED Functions                     ////////////
 /////////////////////////////////////////////////////////////////////////////////////
 
-void LED_Show(int intpos, int width) //color optiosn//)
+void LED_Show(int intpos, int width, int color ) //color optiosn//)
 {
   int i = intpos;
   for (int n = 0; n < width; n++)
   {
-    leds[i] += //COLOR options?????
+    strip.setPixelColor(i, LEDcolor);
+    //leds[i] += //COLOR options?????
     i++;
     if (i = NUM_LEDS)
     {
@@ -139,6 +186,61 @@ void LED_Show(int intpos, int width) //color optiosn//)
 
 void speedToColor()
 {
+  if (GPSspeed <= 3)
+  {
+    LEDcolor = strip.Color(0, 0, 0xFF);
+  }
+  else if (GPSspeed >= 4 && GPSspeed <= 7)
+  {
+    LEDcolor = strip.Color(255, 0, 0);
+  }
+  else if (GPSspeed >= 8 && GPSspeed <= 10)
+  {
+    LEDcolor = strip.Color(0, 0xFF, 0xFF);
+  }
+  else if (GPSspeed >= 11 && GPSspeed <= 13)
+  {
+    LEDcolor = strip.Color(255, 0, 0);
+  }
+  else if (GPSspeed >= 14 && GPSspeed <= 17)
+  {
+    LEDcolor = strip.Color(255, 0, 0);
+  }
+  else if (GPSspeed >= 18 && GPSspeed <= 20)
+  {
+    LEDcolor = strip.Color(255, 0, 0);
+  }
+  else if (GPSspeed >= 21 && GPSspeed <= 23)
+  {
+    LEDcolor = strip.Color(255, 0, 0);
+  }
+  else if (GPSspeed >= 24 && GPSspeed <= 27)
+  {
+    LEDcolor = strip.Color(255, 0, 0);
+  }
+  else if (GPSspeed >= 28)
+  {
+    LEDcolor = strip.Color(255, 0, 0);
+  }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+////////////                        Display Functions                    ////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+void snow_show()
+{
+  display.clearDisplay();
+  display.setCursor(0,0);
+  //display battery voltage, max speed, elevation?
   
+}
+
+void batteryV()
+{
+  float raw_voltage = analogRead(voltagePin);
+  float divider_voltage = (raw_voltage * 3.3) / 1024; //check ADC options here
+  voltage = divider_voltage / (R2/(R1+R2));
 }
 
